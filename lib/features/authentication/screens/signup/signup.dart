4dfd587ec9e089818ec344f6970a0ae5f335597c
vinudev <vinudevtv@gmail.com>
2024-02-20 17:50:59 +0530

    after signup page

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,13 +1,10 @@
 import 'package:babycare/common/widgets/login_signup/Form_divider.dart';
 import 'package:babycare/common/widgets/login_signup/social_button.dart';
 import 'package:babycare/features/authentication/screens/signup/widget/signupform.dart';
-import 'package:babycare/utils/constants/color.dart';
 import 'package:babycare/utils/constants/sizes.dart';
 import 'package:babycare/utils/constants/text_strings.dart';
-import 'package:babycare/utils/helpers/helper_function.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:iconsax/iconsax.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
