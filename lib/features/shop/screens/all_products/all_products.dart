dd659e90ec2e8e9ddf2855e777e57dad8b77b75e
vinudev <vinudevtv@gmail.com>
2024-03-16 10:35:10 +0530

    All Brand , Firebase connection

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,10 +1,7 @@
 import 'package:babycare/common/widgets/appbar/appbar.dart';
-import 'package:babycare/common/widgets/layouts/grid_layout.dart';
-import 'package:babycare/common/widgets/products/product_card/product_card_vertical.dart';
 import 'package:babycare/common/widgets/products/sortable/sortable_products.dart';
 import 'package:babycare/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 
 class AllProducts extends StatelessWidget {
   const AllProducts({super.key});
